194514e9669418ea3b4548f24fa2ff4148ea5edd
shekharsingh-rajput <shekharsingh36912@gmail.com>
2024-10-16 16:02:03 +0530

    Add files via upload

---
+++ make.js
@@ -0,0 +1,203 @@
+// console.log("hii");
+// console.log('shekhar singh');
+// name= 'tony strk';
+// console.log(name);
+// price=55.6
+// console.log(price);
+// x = null
+// console.log(x);
+// y = undefined
+// console.log(y);
+
+// const product = {
+//     tittle : "ball pen",
+//     rating : "4 star",
+//     dealoftheday : "yes",
+//     offer : "5%",
+//     price : "270",
+// };
+// console.log(product);
+// const profile = {
+//     name: "shekhar Singh",
+//     isfollow: true,
+//     post: "190",
+//     follower: "4K",
+//     bio: "student,  learning & creating webpage/application "
+// };
+// console.log(profile);
+/*var shekhar;
+shekhar = prompt("tell me your first name:");
+alert("welcome to our site " + shekhar);*/
+
+// single lime comment
+
+// //arthematic operator
+
+// let a = 5;
+// let b = 4;
+// let c = a+b;
+// console.log("a+b = ",c);
+// console.log("a-b = ",a-b);
+// console.log("a*b = ",a*b);
+// console.log("a/b = ",a/b);
+// console.log("a^b = ",a**b);
+
+// let u=c++;
+// let v=u;
+// console.log(v);
+// let a = 5;
+// a += 5;     //10
+// console.log("a = ",a);
+// a -= 5;     //0
+// console.log("a = ",a);//0
+// a *= 5;     
+// console.log("a = ",a);//25
+// a /= 5;     
+// console.log("a = ",a);//10
+// a %= 5;     
+// console.log("a = ",a);//0
+// a **= 5;     
+// console.log("a = ",a);
+
+// // comparison operator
+
+// let y= 5; //number
+// let b =7;//string
+// console.log("y=b",y==b);
+// console.log("y=b",y!=b);
+// console.log("y=b",y===b);//strict for checking string and number
+// console.log("y>b",y>b);
+// console.log("5<7",y<b);
+// console.log("y<=b",y<=b);
+// console.log("y<=b",y<=b);
+
+// //operator in js
+       //logical AND &&
+       //logical OR ||
+       //logical NOT !
+             //logical operator
+             //  
+
+
+// //conditional statement
+
+// let age = 45;
+
+// if(age >= 18){
+//       console.log(" you can vote",);
+// }
+
+// if(age <= 18){
+//       console.log("you cannot vote");
+// }
+
+
+// let mode = "light";
+// let color;
+
+// if(mode === "dark"){
+//       color = "black";
+// }
+
+// if(mode === "light"){
+//       color ="white";
+// }
+
+// console.log("color =",color);
+
+// //another if else  age code
+
+// let age = "4";
+// if(age >= 18){
+//       console.log("you can vote");
+// }
+// else{console.log("you cannot vote")};
+
+// to find out even/odd number
+
+// let num = "5"
+// if( num % 2 ==  0){
+//       console.log(num," even number");
+// }
+// else{
+//       console.log(num ,"odd number");
+// }
+
+ // // prompt and alert
+
+// let name = prompt(" enter your name");
+// console.log("name - ",name);
+
+//  let num1 = prompt("enter number");
+//  if( num1 % 2 === 0){
+//        console.log(num1 ," is even number");
+//  }
+//  else{console.log(num1," is odd number")};
+
+// let co = prompt("enter number");
+// if (co % 2 === 0) {
+//     console.log("even", co);
+// }
+// else {
+//    console.log("odd", co)
+// };
+
+// Q. //get use to input a number using prompt("enter a number") check if the number is multiple of 5 or not:
+
+// let num2 = prompt("enter number");
+// if(num2 % 3 === 0){
+//       console.log(num2,"is a multiple of 3");
+// }else{
+//       console.log(num2,"is not a multiple of 3")
+// };
+
+//Q.// write a code which can give markss to the student according to their scores;
+
+// let marks = prompt("enter marks and get marks");
+// if (marks >= 80 && marks <= 100) {
+//        grade = "a"
+// } else if (marks > 70 && marks < 79){
+//        grade = "B"
+// }else if(marks > 60 && marks < 69){
+//        grade = "c"
+// }else if(marks > 50 && marks < 59){
+//        grade = "D"
+// }else if( marks >= 0 && marks < 49){
+//        grade = "E"
+// }
+// console.log("your grade is ",grade);
+
+// // for loop 
+
+// for (let i= 1 ; i <= 11 ; i++){
+//       i = i++
+//       console.log("i=",i);
+// }
+
+// // while loop
+
+// let j = 3
+// while (j <= 5) {
+//       console.log("shekhar singh",j)
+//       j++
+// }
+
+// // do while loop
+
+// let a = 10
+// do{
+//       console.log("shekhar singh")
+//       a++;
+// }while( a <= 5) ;
+
+// //for-of-loop
+
+// let str = "shekhar singh";
+
+// let size = 0;
+// for( let i of str){
+//       console.log("i = ",i);
+//       size++;
+// }
+// console.log("size = ",size)
+
